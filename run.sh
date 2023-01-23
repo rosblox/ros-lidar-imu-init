99093d64a6e3a186d3cbf8dae168d6fb73a04759
Max Polzin <polzin.max@gmail.com>
2023-01-23 12:22:53 +0100

    Add xhost+

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+xhost +
+
 docker run -it --rm --privileged --net=host \
 --volume $(pwd)/LiDAR_IMU_Init:/catkin_ws/src/LiDAR_IMU_Init \
 -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
